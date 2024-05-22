bf2e4acbfb30a5c80ef3942d5a26f5c1c12b50ef
Milton José Rocha de Oliveira <157547856+Milton-Rocha-Oliveira@users.noreply.github.com>
2024-05-21 21:22:13 -0300

    Update Metodos.java

--- src/estudo/milton/estudosJava/Metodos.java
+++ src/estudo/milton/estudosJava/Metodos.java
@@ -3,14 +3,14 @@ package estudo.milton.estudosJava;
 public class Metodos {
     public static void main(String [] args){
         System.out.println("Jesus Live");
-        String textoBiblia = bible();
+        String biblicalText = bible();
 
-        String palavraDoDia = "João 3:16...PORQUE DEUS AMOU AO MUNDO DE TAL MANEIRA, QUE DEU SEU FILHO UNIGENITO, PARA QUE TODO AQUELE QUE NELE CRER NÃO PEREÇA MAIS TENHA VIDA ETERNA";
-        System.out.println(palavraDoDia);
-        System.out.println(textoBiblia);
-        System.out.println("Os dois textos biblicos, são iguais ? " + palavraDoDia.equals(textoBiblia));
+        String wordOfTheDay = "John 3:16...FOR GOD SO LOVED THE WORLD, THAT HE GAVE HIS ONLY BEGOTTEN SON, THAT WHOEVER BELIEVES IN HIM SHALL NOT PERISH BUT HAVE ETERNAL LIFE";
+        System.out.println(wordOfTheDay);
+        System.out.println(biblicalText);
+        System.out.println("The two biblical texts are the same ? " + wordOfTheDay.equals(biblicalText));
     }
     public static String bible(){
-        return "João 3:16...PORQUE DEUS AMOU AO MUNDO DE TAL MANEIRA, QUE DEU SEU FILHO UNIGENITO, PARA QUE TODO AQUELE QUE NELE CRER NÃO PEREÇA MAIS TENHA VIDA ETERNA";
+        return "John 3:16...FOR GOD SO LOVED THE WORLD, THAT HE GAVE HIS ONLY BEGOTTEN SON, THAT WHOEVER BELIEVES IN HIM SHALL NOT PERISH BUT HAVE ETERNAL LIFE";
     }
 }
