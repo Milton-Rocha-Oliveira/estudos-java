c27c13ae31ae72931f06bd26a2a1f15e7dbcc2d5
milton rocha <milton.j.r.oliveira.1990@gmail.com>
2024-05-28 21:19:51 -0300

    Merge remote-tracking branch 'origin/main'

--- src/estudo/milton/estudosJava/CandidatosVaga.java
+++ src/estudo/milton/estudosJava/CandidatosVaga.java
@@ -1,3 +1,6 @@
+//ESTE PEQUENO PRAGRAMA CRIA UMA SIMULAÇÃO DE COMO SERIA UM CONTROLE DE FLUXO DURANTE UM PROCESSO SELETIVO EM UMA
+//QUE DESEJA CONTRATAR LEVANDO EM CONSIDERAÇÃO A PRETENÇÃO SALARIAL, AONDE A EMPRESSA DESEJA PARAGR NO MAXIMO 2000
+
 package estudo.milton.estudosJava;
 import java.util.Random;
 import java.util.Scanner;
@@ -11,6 +14,55 @@ public class CandidatosVaga {
             entrandoEmContato(candidato);
         }
     }
+    //FAZ UMA APURAÇÃO ENTRE OS CANDIDATOS, E OS SELECIONA LEVANDO EM CONSIDERAÇÃO AS SUAS PRETEÇÕES SALARIAIS
+    static void selecaoCandidatos(){
+        String [] candidatos = {"Milton", "Juliete", "Eduardo","Otavio","Aparecida","Manoel","Maria"};
+
+        int candidatosSelecionados = 0;
+        int candiatoAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5 && candiatoAtual < candidatos.length){
+            String candidato = candidatos[candiatoAtual];
+            double salarioPrtendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salario " + salarioPrtendido);
+            if(salarioBase >= salarioPrtendido){
+                System.out.println("O candidato "+ candidato + " foi selecionado para vaga.");
+                candidatosSelecionados++;
+            }
+            candiatoAtual++;
+
+        }
+    }
+    //METODO AUXILIAR PARA CRIAR VALORES DE INTENÇÕES SALARIAS FICTICIAS PARA OS CANDIDATOS
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
+    }
+    //ANALIZA SE OS CANDIDATOS ESTÃO DENTRO DA FAIXA SALARIAL QUE A EMPRESSA DESEJA PAGAR
+    static void analizarCandidato(Double salarioPretendido){
+        double salarioBase = 2000.0;
+        if(salarioBase > salarioPretendido){
+            System.out.println("LIGAR CANDIDATO");
+        }else if(salarioBase == salarioPretendido){
+            System.out.println("LIGAR PARA CANDIDATO COM CONTRA PROPOSTA");
+        }else{
+            System.out.println("AGUARDAR RESULTADO DOS DEMAIS");
+        }
+    }
+    //IMPRIME NA TELA OS CANDIDATOS SELECIONADOS POR NOSSO SISTEMA QUE SE BASEI NA INTEÇÃO SALARIAL DO CANDIDATO
+    static void imprimirSelecionados(){
+        String [] candidatos = {"Milton", "Juliete", "Eduardo","Otavio","Aparecida"};
+        System.out.println("Imprimindo lista de candidatos selecionados, informando o indice do elemento.");
+
+        for(int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O candidato selecionado de n " + (indice + 1) + " è o " + candidatos[indice]);
+        }
+        System.out.println("Forma abreviadade interação for each");
+        for(String candidato : candidatos){
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
+    }
+    //ENTRA EM CONTATO COM OS CANDIDATOS QUE ESTÃO DENTRO DA MEDIA SALARIAL QUE A EMPRESA DESEJA
         static void entrandoEmContato(String candidato){
             int  tentativasRealizadas = 1;
             boolean continuarTentando = true;
@@ -30,53 +82,8 @@ public class CandidatosVaga {
                 System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + " NO NUMERO MAXIMO E TENTATIVAS");
             }
         }
-        //metodo auxiliar
+        //METODO AUXILIAR, PARA CRIAR UMA RANDOMIZAÇÃO DE LIGAÇÕES PARA UM TESTE
         static boolean atender(){
             return new Random().nextInt(3)==1;
         }
-
-    static void imprimirSelecionados(){
-        String [] candidatos = {"Milton", "Juliete", "Eduardo","Otavio","Aparecida"};
-        System.out.println("Imprimindo lista de candidatos selecionados, informando o indice do elemento.");
-
-        for(int indice = 0; indice < candidatos.length; indice++) {
-            System.out.println("O candidato selecionado de n " + (indice + 1) + " è o " + candidatos[indice]);
-        }
-            System.out.println("Forma abreviadade interação for each");
-            for(String candidato : candidatos){
-                System.out.println("O candidato selecionado foi " + candidato);
-            }
-    }
-    static void selecaoCandidatos(){
-        String [] candidatos = {"Milton", "Juliete", "Eduardo","Otavio","Aparecida","Manoel","Maria"};
-
-        int candidatosSelecionados = 0;
-        int candiatoAtual = 0;
-        double salarioBase = 2000.0;
-        while(candidatosSelecionados < 5 && candiatoAtual < candidatos.length){
-            String candidato = candidatos[candiatoAtual];
-            double salarioPrtendido = valorPretendido();
-
-            System.out.println("O candidato " + candidato + " solicitou este valor de salario " + salarioPrtendido);
-            if(salarioBase >= salarioPrtendido){
-                System.out.println("O candidato "+ candidato + " foi selecionado para vaga.");
-                candidatosSelecionados++;
-            }
-            candiatoAtual++;
-
-            }
-        }
-    static double valorPretendido(){
-        return ThreadLocalRandom.current().nextDouble(1800,2200);
-    }
-    static void analizarCandidato(Double salarioPretendido){
-        double salarioBase = 2000.0;
-        if(salarioBase > salarioPretendido){
-            System.out.println("LIGAR CANDIDATO");
-        }else if(salarioBase == salarioPretendido){
-            System.out.println("LIGAR PARA CANDIDATO COM CONTRA PROPOSTA");
-        }else{
-            System.out.println("AGUARDAR RESULTADO DOS DEMAIS");
-        }
-    }
 }
